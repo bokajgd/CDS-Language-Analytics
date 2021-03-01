4e8e5c4e689072d99e5b160102617b1c4924ef3e
Jakob Grøhn <bokajgd@gmail.com>
2021-03-01 21:17:30 +0100

    Updating script with better comments

--- W4-Sentiment-Analysis/W4-Sentiment-Analysis.py
+++ W4-Sentiment-Analysis/W4-Sentiment-Analysis.py
@@ -18,9 +18,9 @@ Write a short summary (no more than a paragraph) describing what the two plots s
 
 # Defining main function 
 def main(args):
-    filename = args.filename
+    filename = args.filename # Adding argument that can be specified in command line
 
-    Sentiment(filename = filename) # Change argument input to generate new files with different keywords or window sizes
+    Sentiment(filename = filename) # Calling main class 
 
 # Setting class 'CountFunctions'
 class Sentiment:
@@ -36,15 +36,14 @@ class Sentiment:
             self.filename = "test_data.csv"
 
         df = pd.read_csv(data_dir / f'{self.filename}')  # Read csv file
-        df["Polarity"] = self.get_polarity_score(text = df["headline_text"]) # Adding a column with calculated polarity scores for each headline
+        df["polarity"] = self.get_polarity_score(text = df["headline_text"]) # Adding a column with calculated polarity scores for each headline
 
-        df_grouped_daily = df.groupby('publish_date', as_index=False)['Polarity'].mean() # Grouping headlines and calculating a mean polarity score for each day
-        
-        self.create_plot(out_dir = out_dir, data = df_grouped_daily['Polarity'].rolling(7).mean(), plot_name = "1_Week_Rolling_Average_Polarity")
+        df_grouped_daily = df.groupby('publish_date', as_index=False)['polarity'].mean() # Grouping headlines and calculating a mean score for each day
 
-        self.create_plot(out_dir = out_dir, data = df_grouped_daily['Polarity'].rolling(30).mean(), plot_name = "1_Month_Rolling_Average_Polarity")
+        self.create_plot(out_dir = out_dir, data = df_grouped_daily['polarity'].rolling(7).mean(), plot_name = "Weekly_Rolling_Average_Polarity") # Generating and saving plot of 7-day rolling average sentiment score
 
-        print(df_grouped_daily.head(20))
+        self.create_plot(out_dir = out_dir, data = df_grouped_daily['polarity'].rolling(30).mean(), plot_name = "Monthly_Rolling_Average_Polarity") # Generating and saving plot of 30-day rolling average sentiment score
+
 
     # Defining function for setting directory for the raw data
     def setting_data_directory(self):
@@ -68,21 +67,26 @@ class Sentiment:
 
     # Defining function for calculating polarity
     '''
+    Generates a list containing a polarity score for each input
+    Args:
+        text: Input text
+    Returns:
+        polarity (list): List containing polarity scores for each headline
 
     '''
     def get_polarity_score(self, text):
-        #
-        nlp = spacy.load("en_core_web_sm")
 
-        spacy_text_blob = SpacyTextBlob()
+        nlp = spacy.load("en_core_web_sm") # Initialise spaCy
 
-        nlp.add_pipe(spacy_text_blob)
+        spacy_text_blob = SpacyTextBlob() # Initialise spaCyTextBlob 
 
-        polarity = []
+        nlp.add_pipe(spacy_text_blob) # Add spaCyTextBlob as a new component to our spaCy nlp pipeline
 
-        for headline in nlp.pipe(text):
+        polarity = [] # Creating empy list for 
+
+        for headline in nlp.pipe(text): # Loop through each headline and calculate polarity score
             polarity_score = headline._.sentiment.polarity 
-            polarity.append(float(polarity_score))
+            polarity.append(float(polarity_score)) # Append score to list
 
         return polarity
 
@@ -90,19 +94,19 @@ class Sentiment:
     # Defining function for creating a plot of development in sentiment scores 
     def create_plot(self, out_dir, data, plot_name):
 
-        plt.plot(data, label="Rolling Average Polarity")
+        plt.plot(data, label="Rolling Average Polarity", color = "#3E5B75") # Plot data
 
-        plt.title(plot_name)
+        plt.title(plot_name) # Define a title
 
-        plt.legend(loc='upper right')
+        plt.legend(loc='upper right') # Define legend position
 
-        plt.xlabel('Date')
+        plt.xlabel('Date') # Define x-axis label
 
-        plt.ylabel('Polarity')
+        plt.ylabel('Polarity') # Define y-axis label
 
-        path = out_dir / f"{plot_name}.png"
+        path = out_dir / f"{plot_name}.png" # Define output path using out_dir
 
-        plt.savefig(path)
+        plt.savefig(path) # Save figure
 
         plt.close()
 
@@ -111,7 +115,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--filename',
+    parser.add_argument('--filename', 
                         metavar="Filename",
                         type=str,
                         help='The name of the input data file',
